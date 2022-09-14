2d20bf9e28ed75f04ca7835faf258c2daddd20bb
bendikrs <bendik_sto@hotmail.com>
2022-09-14 19:25:26 +0200

    feil i move

--- tests/robot.py
+++ tests/robot.py
@@ -1,30 +1,42 @@
 import numpy as np
 
 class Robot:
-    def __init__(self, fov, range):
+    def __init__(self, fov, range, x = np.zeros((3, 1))):
         '''
         fov: field of view int degrees
         range: sensing range int meters
+        x0: initial state (x, y, theta)
         '''
         self.fov = fov
         self.range = range
+        self.xTrue = [x] # true state of robot (no noise)
 
     
+    @property
+    def xTrue(self):
+        return self._xTrue
+
+    @xTrue.setter
+    def xTrue(self, x):
+        self._xTrue = x
+
     def move(self, x, u, Rt):
         '''
         Move robot one step, including noise
-        u: control input (v, w)
+        u: control input (rot1, rot2, trans)
         x: state (x, y, theta)
         
         return:
         x: new state (x, y, theta)
         '''
+        u += (np.random.randn(1,3)@Rt)[0]
         
-        x[0] += u[0] * np.cos(x[2])
-        x[1] += u[0] * np.sin(x[2])
-        x[2] += u[1]
-        x[2] = self.wrapToPi(x[2])
+        x[0] += u[0] * np.cos(x[2] + u[1]) # x
+        x[1] += u[0] * np.sin(x[2] + u[1]) # y 
+        x[2] += u[1] + u[2] # theta
+        x[2] = self.wrapToPi(x[2]) 
 
+        self.xTrue.append(u) # save true state
         return x
 
 
