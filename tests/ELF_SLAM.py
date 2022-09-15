e6f646a09a98416ffae0ea201d4b051df90c5fb6
bendikrs <bendik_sto@hotmail.com>
2022-09-15 13:20:44 +0200

    ymse

--- tests/ELF_SLAM.py
+++ tests/ELF_SLAM.py
@@ -1,17 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Map
-seed = 1338
-x_dim, y_dim = 100, 100
-num_landmarks = 10
-landmarks = np.hstack((np.random.RandomState(seed).rand(num_landmarks, 2) * np.array([x_dim, y_dim]), \
-    np.arange(num_landmarks).reshape(-1, 1)))
-fov = 360 # field of view
 
-plt.xlim(0, x_dim)
-plt.ylim(0, y_dim)
+def createLandmarks(v, omega, delta_r, num_landmarks):
+    '''Create map of landmarks, for use on the bonnabel test'''
+    landmarks = np.zeros((num_landmarks, 2))
+    # print(landmarks)
+    r = v/omega
+    for i in range(num_landmarks):
+        rho = r + delta_r
+        theta=(2*np.pi*i)/num_landmarks
+        landmarks[i, 0] = rho*np.cos(theta)
+        landmarks[i, 1] = rho*np.sin(theta)
+
+    return landmarks
+
+
+
+# Map
+num_landmarks = 10
+
+landmarks = createLandmarks(1, 2*np.pi/40, 1, num_landmarks)
 plt.plot(landmarks[:, 0], landmarks[:, 1], 'x')
+plt.show()
 
 Rt = np.array([[0.1,   0,   0], 
                 [  0, 0.1,   0],
@@ -29,10 +40,10 @@ P_hat[3:, 3:] = np.eye(2*num_landmarks)*1e6 # set intial covariance for landmark
 from ekf import EKF
 from robot import Robot
 ekf = EKF()
-robot = Robot(fov, 100)
+robot = Robot(5)
 
 # simulate
-u = np.array([1, 0])
+u = np.array([0, 0, 1])
 while True:
     # move robot
     # predict
@@ -42,7 +53,7 @@ while True:
     # 
 
     # move
-    # x = robot.move(x, u, Rt)
+    x = robot.move(x, u, Rt)
     # predict
     x_hat, P_hat = ekf.predict(x_hat, u, P_hat, Rt)
     # sense
