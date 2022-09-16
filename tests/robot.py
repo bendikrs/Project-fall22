0b7f2b535390aab7ec83b8494fea5c3553c3b49c
simengronli <simenggronli@hotmail.com>
2022-09-16 19:39:41 +0200

    xTrue

--- tests/robot.py
+++ tests/robot.py
@@ -1,22 +1,22 @@
 import numpy as np
 
 class Robot:
-    def __init__(self, range, x = np.zeros((3, 1))):
+    def __init__(self, range, x = np.zeros(3)):
         '''
         range: sensing range int meters
         x0: initial state (x, y, theta)
         '''
         self.range = range
-        self.xTrue = [x] # true state of robot (no noise)
+        self.xTrue = x # true state of robot (no noise)
 
     
-    @property
-    def xTrue(self):
-        return self._xTrue
+    # @property
+    # def xTrue(self):
+    #     return self._xTrue
 
-    @xTrue.setter
-    def xTrue(self, x):
-        self._xTrue = x
+    # @xTrue.setter
+    # def xTrue(self, x):
+    #     self._xTrue = x
 
     def move(self, x, u, Rt):
         '''
@@ -27,15 +27,18 @@ class Robot:
         return:
         x: new state (x, y, theta)
         '''
-        randMat = np.random.randn(1,2).astype("float64")
         u = u.astype("float64")
+        self.xTrue[0] += u[0]*np.cos(self.xTrue[2] + u[1]) 
+        self.xTrue[1] += u[0]*np.sin(self.xTrue[2] + u[1]) 
+        self.xTrue[2] += u[1]
+        self.xTrue[2] = self.wrapToPi(self.xTrue[2])
+
+        randMat = np.random.randn(1,2).astype("float64")
         u += (randMat@Rt[:2,:2])[0]
         x[0] += u[0] * np.cos(x[2] + u[1]) # x
         x[1] += u[0] * np.sin(x[2] + u[1]) # y 
         x[2] += u[1] # theta
         x[2] = self.wrapToPi(x[2]) 
-
-        self.xTrue.append(u) # save true state
         return x
 
 
