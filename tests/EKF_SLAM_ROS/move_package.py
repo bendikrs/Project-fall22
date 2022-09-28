bf1848515d73754595efcb966db6e7e9d9f9d121
bendikrs <bendik_sto@hotmail.com>
2022-09-28 11:50:37 +0200

    movepkg

---
+++ tests/EKF_SLAM_ROS/move_package.py
@@ -0,0 +1,13 @@
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--username', type=str, required=True)
+args = parser.parse_args()
+
+
+original = r'/home/' + repr(args.username)[1:-1] + r'/turtlebot3_ws/src'
+target = r'/home/' + repr(args.username)[1:-1] + r'/git/Project-fall22/tests/EKF_SLAM_ROS/ros2_package'
+
+shutil.copytree(original, target)
