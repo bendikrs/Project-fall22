4e716ab50b556cfb47b7d691e152a3c6c1e577cb
bendikrs <bendik_sto@hotmail.com>
2022-09-30 13:21:34 +0200

    dbscan

--- tests/EKF_SLAM_ROS/ros2_package/ekf_slam_ros2/ekf_slam_ros2/EKF_SLAM.py
+++ tests/EKF_SLAM_ROS/ros2_package/ekf_slam_ros2/ekf_slam_ros2/EKF_SLAM.py
@@ -1,5 +1,13 @@
 import numpy as np
+from sklearn import cluster
 from sklearn.cluster import DBSCAN
+
+# not in setup.py
+from collections import Counter
+import circlehough.hough as hough
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+# ---
 
 import rclpy
 from rclpy.node import Node
@@ -11,6 +19,11 @@ class EKF_SLAM(Node):
 
     def __init__(self):
         super().__init__('EKF_SLAM')
+        self.fig, self.ax = plt.subplots()
+
+        # EKF
+        self.x = np.array([0, 0, 0])
+        self.P = np.eye(3)
 
         # subscriber
         self.subscription = self.create_subscription(
@@ -33,29 +46,37 @@ class EKF_SLAM(Node):
         ranges = msg.ranges # list of ranges
         angle_min = msg.angle_min # start angle
         angle_max = msg.angle_max # end angle
-        angle_increment = msg.angle_increment # angle increment between scans (rad)
-        time_increment = msg.time_increment # time increment between measurements (sec)
-        scan_time = msg.scan_time # time between scans (sec)
-        range_min = msg.range_min # minimum range value (m)
-        range_max = msg.range_max # maximum range value (m)
-
-        # remove inf and nan
-        ranges = [x for x in ranges if not np.isinf(x) and not np.isnan(x)]
-        ranges = np.array(ranges)
 
         # make cartesian coordinates
         theta = np.linspace(angle_min, angle_max, len(ranges))
         x = ranges * np.cos(theta)
         y = ranges * np.sin(theta)
 
+        # remove inf and nan
+        x = [i for i in x if not np.isinf(i) and not np.isnan(i)]
+        y = [i for i in y if not np.isinf(i) and not np.isnan(i)]
+        point_cloud = np.array([x, y]).T
+
         # clustering with DBSCAN
-        X = np.array([x, y]).T
-        db = DBSCAN(eps=0.5, min_samples=10).fit(X)
-        labels = db.labels_
+        db = DBSCAN().fit(point_cloud)
 
+        # make array of clusters
+        clusters = []
+        for i in range(len(db.components_)):
+            clusters.append(point_cloud[db.labels_ == i])
 
-        self.get_logger().info('Clusters: "%s"' % labels)
+        landmarks = []
+        for cluster in clusters:
+            self.ax.scatter(cluster[:,0], cluster[:,1])
 
+            if len(cluster) > 3 and len(cluster) < 100:
+                guessed_cx = np.mean(cluster[:,0])
+                guessed_cy = np.mean(cluster[:,1])
+                self.ax.add_patch(patches.Circle((guessed_cx, guessed_cy), 0.125, fill=False, color='red'))         
+                landmarks.append(np.array([guessed_cx, guessed_cy]))
+
+        plt.pause(0.5)
+        plt.cla()
 
 
     def timer_callback(self):
