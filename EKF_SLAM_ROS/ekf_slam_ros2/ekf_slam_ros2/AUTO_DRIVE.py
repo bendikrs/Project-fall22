4752d30eb9ec1c5e3368a71382197cb009f7bdff
simengronli <simenggronli@hotmail.com>
2022-11-22 15:23:23 +0100

    data_output from live demo

--- EKF_SLAM_ROS/ekf_slam_ros2/ekf_slam_ros2/AUTO_DRIVE.py
+++ EKF_SLAM_ROS/ekf_slam_ros2/ekf_slam_ros2/AUTO_DRIVE.py
@@ -9,15 +9,21 @@ from geometry_msgs.msg import Twist
 class AutoDrive(Node):
     def __init__(self):
         super().__init__('auto_drive')
+        timer_period = 5 # seconds
+        self.linear_velocity = 0.1
+        self.angular_velocity = self.radius2angular_velocity(0.65)
+
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         msg = Twist()
-        msg.linear.x = 0.1
-        msg.angular.z = 0.14
+        msg.linear.x = self.linear_velocity
+        msg.angular.z = self.angular_velocity
         self.publisher_.publish(msg)
+
+    def radius2angular_velocity(self, radius):
+        return self.linear_velocity / radius
 
 def main(args=None):
     rclpy.init(args=args)
