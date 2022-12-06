25d58231ab617ec882cf0cd692a12edf784bace4
bendikrs <bendik_sto@hotmail.com>
2022-12-06 16:08:00 +0100

    rmse gazebo plot

--- visualization/RMSE_plotting.py
+++ visualization/RMSE_plotting.py
@@ -13,16 +13,19 @@ for file_name in file_names:
     if file_name.endswith(".csv"):
         dataframes[file_name[:-4]] = pd.read_csv(path + file_name, skiprows=lambda x: (x != 0) and not x % 2)
 
-# print(dataframes["RMSE_pose0_heading1_LIVE_DEMO"])
 
-# print(dataframes["RMSE_pose0_heading1_GAZEBO"])
+
+pose_real_world_file =  "RMSE_pose0_heading1_LIVE_DEMO"
+pose_gazebo_file = "RMSE_pose0_heading1_GAZEBO_mahlanobis"
+
 # Plot the data
 # Pose RMSE
 fig, ax = plt.subplots(figsize=(5, 5))
-ax.plot(dataframes["RMSE_pose0_heading1_GAZEBO"]["__time"]
-   - dataframes["RMSE_pose0_heading1_GAZEBO"]["__time"][0], dataframes["RMSE_pose0_heading1_GAZEBO"]["/RMSE/data.0"])
-ax.plot(dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["__time"]
-   - dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["__time"][0], dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["/RMSE/data.0"])
+ax.plot(dataframes[pose_gazebo_file]["__time"]
+   - dataframes[pose_gazebo_file]["__time"][0], dataframes[pose_gazebo_file]["/RMSE/data.0"])
+
+ax.plot(dataframes[pose_real_world_file]["__time"]
+   - dataframes[pose_real_world_file]["__time"][0], dataframes[pose_real_world_file]["/RMSE/data.0"])
 ax.set_title("Pose RMSE")
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("RMSE (m)")
@@ -33,11 +36,11 @@ plt.show()
 
 # Heading RMSE
 fig, ax = plt.subplots(figsize=(5, 5))
-ax.plot(dataframes["RMSE_pose0_heading1_GAZEBO"]["__time"] 
-    - dataframes["RMSE_pose0_heading1_GAZEBO"]["__time"][0], dataframes["RMSE_pose0_heading1_GAZEBO"]["/RMSE/data.1"])
+ax.plot(dataframes[pose_gazebo_file]["__time"] 
+    - dataframes[pose_gazebo_file]["__time"][0], dataframes[pose_gazebo_file]["/RMSE/data.1"])
 
-ax.plot(dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["__time"] 
-    - dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["__time"][0], dataframes["RMSE_pose0_heading1_LIVE_DEMO"]["/RMSE/data.1"])
+ax.plot(dataframes[pose_real_world_file]["__time"] 
+    - dataframes[pose_real_world_file]["__time"][0], dataframes[pose_real_world_file]["/RMSE/data.1"])
 ax.set_title("Heading RMSE")
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("RMSE (rad)")
